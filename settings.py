6245d5b337da0f13228ff9083a3b506763a903f7
yiquanfeng <yiquanfeng@qq.com>
2026-01-19 14:28:32 +0800

    add token validate for latest napcat and safe

--- settings.py
+++ settings.py
@@ -1,7 +1,11 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    http_url: str = "http://10.29.237.29:3001/"
-    ws_url: str = "ws://10.29.237.29:3000/"
+    http_url: str = "http://localhost:3001/"
+    ws_url: str = "ws://localhost:3001/"
+    token: str = "110119"
+    header: dict = {
+        "Authorization": f"{token}"
+    }
 
 settings = Settings()
